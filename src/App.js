070a3f36dd92c011a868e761e7277a445d1a1769
Rishabh Jain <rishabhjms99@gmail.com>
2022-12-06 13:11:53 +0530

    Changing title dynamically & adding favicons to TextUtils

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
 import './style.css';
+import Alert from './components/Alert';
 function App() {
+  const [mode, setMode] = useState('light'); //Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1400);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#13202d'
+      showAlert("Dark Mode was enabled","success")
+      document.title = 'Textutils - Dark Mode'
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light Mode was enabled","success")
+      document.title = 'Textutils - Light Mode'
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyse below" />
+        <TextForm heading="Enter the text to analyse below" mode={mode} showAlert={showAlert}/>
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,24 +2,42 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        console.log("Uppercase was clicked" + text)
         let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to Uppercase",'success')
+    }
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to Lowercase",'success')
+    }
+    const clearText = () => {
+        let newText = ' '
         setText(newText)
     }
     const handleOnChange = (e) => {
         console.log("On Change")
         setText(e.target.value)
     }
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // text = "New text" // Invalid way to alter default text in useState
     // setText("New text"); // Invalid way to alter default text in useState
     return (
         <>
-            <h1>{props.heading}</h1>
+            <h2 style={{color: props.mode==='dark'?'white':'black'}}>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{
+                    backgroundColor: props.mode==='dark'?'grey':'white'
+                }}></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={clearText}>Clear Text</button>
+            <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+                <p>Your text has <b>{text.length}</b> characters and <b>{text.split(" ").length}</b> words</p>
+                <h2>Your text preview</h2>
+                <p>{text.length>0?text:"Enter something in the textbox above to preview"}</p>
+            </div>
         </>
     )
 }
