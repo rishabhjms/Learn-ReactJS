b34b76900c8298d011a8d1113300d7f992d49a44
Rishabh Jain <rishabhjms99@gmail.com>
2022-12-22 19:58:00 +0530

    Adding Previous and Next Buttons to our App

--- NewsMonkey/src/components/Navbar.js
+++ NewsMonkey/src/components/Navbar.js
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
   render() {
     return (
         <>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- NewsMonkey/src/components/News.js
+++ NewsMonkey/src/components/News.js
@@ -6,17 +6,46 @@ export default class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 0
     }
+
   }
-  async componentDidMount(){
-    let url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-11-19&sortBy=publishedAt&apiKey=cff3bd233fd64345916309703a080029'
+
+  async componentDidMount() {
+    let url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-11-23&sortBy=publishedAt&apiKey=cff3bd233fd64345916309703a080029&page=1'
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData)
     this.setState({
+      articles: parsedData.articles,
+    })
+
+  }
+  handPrevClick = async () => {
+    let url = `https://newsapi.org/v2/everything?q=tesla&from=2022-11-23&sortBy=publishedAt&apiKey=cff3bd233fd64345916309703a080029&page=${this.state.page - 1}&pageSize=20`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData)
+    this.setState({
+      page: this.state.page + 1,
       articles: parsedData.articles
     })
+  }
+  handNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+    } else {
+      let url = `https://newsapi.org/v2/everything?q=tesla&from=2022-11-23&sortBy=publishedAt&apiKey=cff3bd233fd64345916309703a080029&page=${this.state.page + 1}&pageSize=20`
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData)
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults
+      })
+    }
   }
   render() {
     console.log("Hello I am render function")
@@ -27,10 +56,14 @@ export default class News extends Component {
           {
             this.state.articles.map((e) => {
               return <div className="col-md-4" key={e.url} >
-                <Newsitem title={e.title?e.title:""} description={e.description?e.title:"..."} imgUrl={e.urlToImage===null?e.urlToImage='https://i.insider.com/5dfba45ee94e860d59646b66?width=1200&format=jpeg':e.urlToImage} newsURL={e.url} />
+                <Newsitem title={e.title ? e.title : ""} description={e.description ? e.title : "..."} imgUrl={e.urlToImage === null ? e.urlToImage = 'https://i.insider.com/5dfba45ee94e860d59646b66?width=1200&format=jpeg' : e.urlToImage} newsURL={e.url} />
               </div>
             })
           }
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button className="btn btn-dark" disabled={this.state.page <= 1} onClick={this.handPrevClick}>&larr; Previous</button>
+          <button className="btn btn-dark" onClick={this.handNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
